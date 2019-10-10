4f58b10d617d6d81eb87c2b54d761e6e9e6e9dda
tojo <aud.jeanba@gmail.com>
2019-10-10 16:57:40 +0300

    Expense design

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  text = "";
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  // onChangeText() {
+  //   this.text = "Changed!";
+  // }
+  saveAdd() {
+    this.text = "Add button click";
+    alert(this.text);
+    console.log("Add");
   }
 
+  clear() {
+    this.text = "Clear button click";
+    alert(this.text);
+    console.log("Clear");
+  }
 }
